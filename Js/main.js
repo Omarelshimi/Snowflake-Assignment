d64dae911a03b3ca10355ce78bc563aeb2f7ff50
Omar Elshimi <70725794+Omarelshimi@users.noreply.github.com>
2021-06-02 12:56:05 -0600

    Add files via upload

---
+++ Js/main.js
@@ -0,0 +1,15 @@
+// Snowflake Simulator
+
+// Set up Canvas and Graphics Context
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d");
+initGraphics(800, 600);
+
+let snowflakes = [];
+
+function snowflakes() {
+    for (let i = 0; i < 50; i++) {
+        
+    }
+}
+
